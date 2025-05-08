b7799cc5de7792cce5ebe781d0306c1397b45c89
violaine soulas <vhsoulas@gmail.com>
2025-05-08 09:54:38 +0200

    Corrections

--- app/controllers/job_load_evaluations_controller.rb
+++ app/controllers/job_load_evaluations_controller.rb
@@ -4,8 +4,11 @@ class JobLoadEvaluationsController < ApplicationController
   after_action :verify_policy_scoped, only: :index  # Vérifie la politique du scope pour l'index
 
   def index
-    @player_profils = PlayerProfil.all
-    @job_load_evaluations = policy_scope(JobLoadEvaluation)
+
+      @player_profils = PlayerProfil.all
+      @job_load_evaluations = policy_scope(JobLoadEvaluation).includes(user: :player_profil) # Évite les requêtes N+1
+
+
 end
 
   def show
@@ -17,14 +20,19 @@ end
     @player_profils = PlayerProfil.all
     authorize @job_load_evaluation
   end
-
   def create
     @job_load_evaluation = JobLoadEvaluation.new(job_load_evaluation_params)
     authorize @job_load_evaluation
-    # Associe l'évaluation de charge de travail à l'utilisateur actuel
-    @job_load_evaluation.user = current_user
+
+    # Associe l'évaluation de charge de travail à l'utilisateur actuel ou à un joueur spécifique
+    @job_load_evaluation.user = if current_user.entraineur?
+                                  User.find(job_load_evaluation_params[:user_id]) # Associe le joueur sélectionné par l'entraîneur
+                                else
+                                  current_user # Si c'est le joueur lui-même, on utilise l'utilisateur connecté
+                                end
+
     if @job_load_evaluation.save
-      redirect_to @job_load_evaluation, notice: 'Job load evaluation was successfully created.'
+      redirect_to @job_load_evaluation, notice: 'L’évaluation a été créée avec succès.'
     else
       render :new, status: :unprocessable_entity
     end
