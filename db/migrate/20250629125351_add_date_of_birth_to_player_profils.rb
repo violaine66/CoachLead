8467b6eb95316a34758fb0bd48f58477b3907819
violaine soulas <vhsoulas@gmail.com>
2025-06-29 16:46:21 +0200

    Ajout condition dans migration sur l'ajout colonne birth date

--- db/migrate/20250629125351_add_date_of_birth_to_player_profils.rb
+++ db/migrate/20250629125351_add_date_of_birth_to_player_profils.rb
@@ -1,5 +1,6 @@
 class AddDateOfBirthToPlayerProfils < ActiveRecord::Migration[7.1]
   def change
+    unless column_exists?(:player_profils, :date_of_birth)
     add_column :player_profils, :date_of_birth, :date
   end
 end
