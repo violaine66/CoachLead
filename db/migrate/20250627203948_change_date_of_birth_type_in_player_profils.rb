1ee894f18a3309181fa4cf1e507242f65fdd87eb
violaine soulas <vhsoulas@gmail.com>
2025-06-29 16:39:30 +0200

    Date of birth en mode date + correction de pb dans le form new PP

--- db/migrate/20250627203948_change_date_of_birth_type_in_player_profils.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDateOfBirthTypeInPlayerProfils < ActiveRecord::Migration[7.1]
-    def up
-    change_column :player_profils, :date_of_birth, :string
-  end
-
-  def down
-    change_column :player_profils, :date_of_birth, :date
-  end
-end
--- db/migrate/20250629124439_remove_date_of_birth_from_player_profils.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDateOfBirthFromPlayerProfils < ActiveRecord::Migration[7.1]
-  def change
-        remove_column :player_profils, :date_of_birth
-  end
-end
