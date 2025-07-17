f6b9c5d0ebbe26e2c096948fa5898067796177c1
violaine soulas <vhsoulas@gmail.com>
2025-07-17 11:31:02 +0200

    Ajout validation sur 10 pour note de match dans modele et edit du matchperformances

--- app/models/match_performance.rb
+++ app/models/match_performance.rb
@@ -6,6 +6,7 @@ class MatchPerformance < ApplicationRecord
   validates :yellow_card, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :played, inclusion: { in: [true, false] }
   validates :match_date, presence: true
+  validates :note_match, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }, allow_nil: true
 
     scope :for_player_on_date, ->(user_id, match_date) {
     where(user_id: user_id, match_date: match_date)
